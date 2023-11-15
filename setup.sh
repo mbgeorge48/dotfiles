3fa6edf7b6ef1db8969d64f0977b926273b7bb73
Matt George <mbgeorge48@gmail.com>
2023-11-15 09:13:12 +0000

    Start moving scripts into their own files

--- setup.sh
+++ setup.sh
@@ -53,83 +53,83 @@ function create_custom_aliases() {
     fi
 }
 
-function brew_setup() {
-    type brew &>/dev/null
-    if [ $? -ne 0 ]; then
-        colour_echo "Installing Brew"
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-        colour_echo "Finished installing Brew, installing Brew packages"
-        brew bundle --file ~/.dotfiles/brew/Brewfile
-    else
-        colour_echo "Skipping Brew, looks like it's already installed,
-        installing packages now"
-    fi
-}
+# function brew_setup() {
+#     type brew &>/dev/null
+#     if [ $? -ne 0 ]; then
+#         colour_echo "Installing Brew"
+#         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#         eval "$(/opt/homebrew/bin/brew shellenv)"
+#         colour_echo "Finished installing Brew, installing Brew packages"
+#         brew bundle --file ~/.dotfiles/brew/Brewfile
+#     else
+#         colour_echo "Skipping Brew, looks like it's already installed,
+#         installing packages now"
+#     fi
+# }
 
-function git_setup() {
-    colour_echo "Configuring Git"
-    if [ -s ~/.gitconfig ]; then
-        mv ~/.gitconfig ~/.dotfiles/archive/ &>/dev/null
-    fi
-    ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-}
+# function git_setup() {
+#     colour_echo "Configuring Git"
+#     if [ -s ~/.gitconfig ]; then
+#         mv ~/.gitconfig ~/.dotfiles/archive/ &>/dev/null
+#     fi
+#     ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+# }
 
-function work_git_setup() {
-    colour_echo "Copying gitconfig-extended into work folder"
-    mkdir -p ~/work/
-    if [ -s ~/work/.gitconfig-work ]; then
-        mv ~/work/.gitconfig-work ~/.dotfiles/archive/ &>/dev/null
-    fi
-    cp ~/.dotfiles/git/gitconfig-extended ~/work/.gitconfig-work
-}
+# function work_git_setup() {
+#     colour_echo "Copying gitconfig-extended into work folder"
+#     mkdir -p ~/work/
+#     if [ -s ~/work/.gitconfig-work ]; then
+#         mv ~/work/.gitconfig-work ~/.dotfiles/archive/ &>/dev/null
+#     fi
+#     cp ~/.dotfiles/git/gitconfig-extended ~/work/.gitconfig-work
+# }
 
-function personal_git_setup() {
-    colour_echo "Configuring personal gitconfig"
-    mkdir -p ~/personal/
-    if [ -s ~/personal/.gitconfig-personal ]; then
-        mv ~/personal/.gitconfig-personal ~/.dotfiles/archive/ &>/dev/null
-    fi
-    ln -s ~/.dotfiles/git/gitconfig-extended ~/personal/.gitconfig-personal
+# function personal_git_setup() {
+#     colour_echo "Configuring personal gitconfig"
+#     mkdir -p ~/personal/
+#     if [ -s ~/personal/.gitconfig-personal ]; then
+#         mv ~/personal/.gitconfig-personal ~/.dotfiles/archive/ &>/dev/null
+#     fi
+#     ln -s ~/.dotfiles/git/gitconfig-extended ~/personal/.gitconfig-personal
 
-    if [ ! -s ~/personal/dotfiles ]; then
-        ln -s ~/.dotfiles ~/personal/dotfiles
-    fi
-}
+#     if [ ! -s ~/personal/dotfiles ]; then
+#         ln -s ~/.dotfiles ~/personal/dotfiles
+#     fi
+# }
 
-function vim_setup() {
-    colour_echo "Configuring Vim"
-    if [ -s ~/.vimrc ]; then
-        mv ~/.vimrc ~/.dotfiles/archive/ &>/dev/null
-    fi
-    ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-}
+# function vim_setup() {
+#     colour_echo "Configuring Vim"
+#     if [ -s ~/.vimrc ]; then
+#         mv ~/.vimrc ~/.dotfiles/archive/ &>/dev/null
+#     fi
+#     ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+# }
 
-function vscode_setup() {
-    colour_echo "Configuring VS Code"
-    if [ -s ~/Library/Application\ Support/Code/User/settings.json ]; then
-        mv ~/Library/Application\ Support/Code/User/settings.json ~/.dotfiles/archive/ &>/dev/null
-    fi
-    ln -s ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+# function vscode_setup() {
+#     colour_echo "Configuring VS Code"
+#     if [ -s ~/Library/Application\ Support/Code/User/settings.json ]; then
+#         mv ~/Library/Application\ Support/Code/User/settings.json ~/.dotfiles/archive/ &>/dev/null
+#     fi
+#     ln -s ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 
-    colour_echo "Installing VS Code extensions"
-    . ~/.dotfiles/vscode/extensions
-}
+#     colour_echo "Installing VS Code extensions"
+#     . ~/.dotfiles/vscode/extensions
+# }
 
-function pyenv_setup() {
-    colour_echo "Configuring Pyenv"
-    run_with_privileges apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
-    curl https://pyenv.run | bash
-}
+# function pyenv_setup() {
+#     colour_echo "Configuring Pyenv"
+#     run_with_privileges apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+#     curl https://pyenv.run | bash
+# }
 
-function npm_setup() {
-    colour_echo "Configuring NPM"
-    run_with_privileges apt-get install nodejs npm -y
-    colour_echo "Install Yarn"
-    run_with_privileges npm install -g yarn
-    colour_echo "Configuring NVM"
-    curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-}
+# function npm_setup() {
+#     colour_echo "Configuring NPM"
+#     run_with_privileges apt-get install nodejs npm -y
+#     colour_echo "Install Yarn"
+#     run_with_privileges npm install -g yarn
+#     colour_echo "Configuring NVM"
+#     curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+# }
 
 # Create ssh and gpg folders
 function gpg_ssh_setup() {
